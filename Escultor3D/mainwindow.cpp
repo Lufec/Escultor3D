1c1a45fb230b16e9ee33ebc15ccfd098dea1d284
Lufec <30414428+Lufec@users.noreply.github.com>
2019-06-09 14:01:08 -0300

    Add alpha version

---
+++ Escultor3D/mainwindow.cpp
@@ -0,0 +1,237 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "sculptor.h"
+#include "plotter.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    //////////////////////////////////
+    connect(ui->pushButtonPV,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(changeShape1()));
+
+    connect(ui->pushButtonCV,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(changeShape2()));
+
+    connect(ui->pushButtonPB,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(changeShape3()));
+
+    connect(ui->pushButtonCB,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(changeShape4()));
+
+    connect(ui->pushButtonPS,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(changeShape5()));
+
+    connect(ui->pushButtonCS,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(changeShape6()));
+
+    connect(ui->pushButtonPE,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(changeShape7()));
+
+    connect(ui->pushButtonCE,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(changeShape8()));
+
+    connect(ui->pushButtonXY,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(changePlane1()));
+
+    connect(ui->pushButtonXZ,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(changePlane2()));
+
+    connect(ui->pushButtonYZ,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(changePlane3()));
+
+    ///////////////////////////////////////////
+
+    connect(ui->horizontalSliderX,
+            SIGNAL(valueChanged(int)),
+            ui->widgetPlotter,
+            SLOT(changeSizeX(int)));
+
+    connect(ui->horizontalSliderY,
+            SIGNAL(valueChanged(int)),
+            ui->widgetPlotter,
+            SLOT(changeSizeY(int)));
+
+    connect(ui->horizontalSliderZ,
+            SIGNAL(valueChanged(int)),
+            ui->widgetPlotter,
+            SLOT(changeSizeZ(int)));
+
+    connect(ui->horizontalSliderRD,
+            SIGNAL(valueChanged(int)),
+            ui->widgetPlotter,
+            SLOT(changeRadius(int)));
+
+    connect(ui->horizontalSliderRX,
+            SIGNAL(valueChanged(int)),
+            ui->widgetPlotter,
+            SLOT(changeRadiusX(int)));
+
+    connect(ui->horizontalSliderRY,
+            SIGNAL(valueChanged(int)),
+            ui->widgetPlotter,
+            SLOT(changeRadiusY(int)));
+
+    connect(ui->horizontalSliderRZ,
+            SIGNAL(valueChanged(int)),
+            ui->widgetPlotter,
+            SLOT(changeRadiusZ(int)));
+
+    connect(ui->horizontalSliderRed,
+            SIGNAL(valueChanged(int)),
+            ui->widgetPlotter,
+            SLOT(changeRed(int)));
+
+    connect(ui->horizontalSliderGreen,
+            SIGNAL(valueChanged(int)),
+            ui->widgetPlotter,
+            SLOT(changeGreen(int)));
+
+    connect(ui->horizontalSliderBlue,
+            SIGNAL(valueChanged(int)),
+            ui->widgetPlotter,
+            SLOT(changeBlue(int)));
+
+    connect(ui->horizontalSliderAlpha,
+            SIGNAL(valueChanged(int)),
+            ui->widgetPlotter,
+            SLOT(changeAlpha(int)));
+
+    connect(ui->horizontalSliderSlice,
+            SIGNAL(valueChanged(int)),
+            ui->widgetPlotter,
+            SLOT(changeSlice(int)));
+
+   /////////////////////////////////////////////
+
+    connect(ui->widgetPlotter,
+            SIGNAL(clickX(int)),
+            ui->lcdNumberX,
+            SLOT(display(int)));
+
+    connect(ui->widgetPlotter,
+            SIGNAL(clickY(int)),
+            ui->lcdNumberY,
+            SLOT(display(int)));
+
+    connect(ui->widgetPlotter,
+            SIGNAL(mouseLinha(int)),
+            ui->lcdNumberLinha,
+            SLOT(display(int)));
+
+    connect(ui->widgetPlotter,
+            SIGNAL(mouseColuna(int)),
+            ui->lcdNumberColuna,
+            SLOT(display(int)));
+/////////////////////////////////////////
+
+    connect(ui->pushButtonOFF,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(saveOFF()));
+
+    connect(ui->pushButtonVECT,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(saveVECT()));
+
+    connect(ui->pushButtonSV,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(saveAll()));
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::changePlane1() //XY
+{
+    ui->widgetPlotter->plane = 1;
+    emit ui->widgetPlotter->planeChosen(1);
+}
+void MainWindow::changePlane2() //XZ
+{
+    ui->widgetPlotter->plane = 2;
+    emit ui->widgetPlotter->planeChosen(2);
+}
+void MainWindow::changePlane3() //YZ
+{
+    ui->widgetPlotter->plane = 3;
+    emit ui->widgetPlotter->planeChosen(3);
+}
+
+void MainWindow::changeShape1() //PV
+{
+    ui->widgetPlotter->shape = 1;
+}
+void MainWindow::changeShape2() //CV
+{
+    ui->widgetPlotter->shape = 2;
+}
+void MainWindow::changeShape3() //PB
+{
+    ui->widgetPlotter->shape = 3;
+}
+void MainWindow::changeShape4() //CB
+{
+    ui->widgetPlotter->shape = 4;
+}
+void MainWindow::changeShape5() //PS
+{
+    ui->widgetPlotter->shape = 5;
+}
+void MainWindow::changeShape6() //CS
+{
+    ui->widgetPlotter->shape = 6;
+}
+void MainWindow::changeShape7() //PE
+{
+    ui->widgetPlotter->shape = 7;
+}
+void MainWindow::changeShape8() //PS
+{
+    ui->widgetPlotter->shape = 8;
+}
+
+
+void MainWindow::saveVECT()
+{
+    ui->widgetPlotter->cube->writeVECT("/home/lufec/Escultor3D/file.vect");
+}
+void MainWindow::saveOFF()
+{
+    ui->widgetPlotter->cube->writeOFF("/home/lufec/Escultor3D/file.off");
+}
+void MainWindow::saveAll()
+{
+    ui->widgetPlotter->cube->writeVECT("/home/lufec/Escultor3D/file.vect");
+    ui->widgetPlotter->cube->writeOFF("/home/lufec/Escultor3D/file.off");
+}
---
+++ Escultor3D/plotter.h
@@ -0,0 +1,77 @@
+#ifndef PLOTTER_H
+#define PLOTTER_H
+
+#include <QWidget>
+#include <QAction>
+#include <QColor>
+#include <vector>
+#include "sculptor.h"
+
+class Plotter : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Plotter(QWidget *parent = nullptr);
+    void paintEvent(QPaintEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+
+    void drawShape(int shape);
+
+    int plane;
+    int shape;
+    Sculptor *cube;
+private:
+    std::vector<std::vector<Voxel>> m;
+    QColor lineColor;
+    QAction *actionMudaCor;
+
+    int scpSizeX, scpSizeY, scpSizeZ;
+
+    int colorRed;
+    int colorGreen;
+    int colorBlue;
+    int transparency;
+
+    int sizeX,sizeY,sizeZ;
+    int radius, radiusX, radiusY,radiusZ;
+
+    bool mousePressed;
+    int mouseX,mouseY;
+
+    int posX,posY,posZ;
+
+    int slice;
+
+    float sizeSquareX,sizeSquareY;
+
+signals:
+  void moveX(int);
+  void moveY(int);
+  void clickX(int);
+  void clickY(int);
+  void mouseLinha(int);
+  void mouseColuna(int);
+  void planeChosen(int);
+
+public slots:
+
+  void changeSizeX(int size);
+  void changeSizeY(int size);
+  void changeSizeZ(int size);
+  void changeRadius(int rd);
+  void changeRadiusX(int rx);
+  void changeRadiusY(int ry);
+  void changeRadiusZ(int rz);
+
+  void changeRed(int red);
+  void changeGreen(int green);
+  void changeBlue(int blue);
+  void changeAlpha(int alpha);
+
+  void changeSlice(int pln);
+
+};
+
+#endif // PLOTTER_H
